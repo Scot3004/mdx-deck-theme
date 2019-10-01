9765e6c22d3d728de0cb5e7cdbd5ba1bb46b1f74
Sergio C. Orozco Torres <scot3004@gmail.com>
2019-10-01 06:02:15 -0500

    Avances en plugins starters y comparativa

--- example/deck.js
+++ example/deck.js
@@ -1,22 +1,15 @@
 import intro from './topics/intro.mdx'
 import gatsbyOverview from './topics/gatsby-overview.mdx'
-import newFeatures from './topics/new-features.mdx'
+import features from './topics/features.mdx'
 import demo from './topics/demo.mdx'
 import qAndA from './topics/q-and-a.mdx'
 import conclusion from './topics/conclusion.mdx'
-
-// import './fonts/Webfonts/futurapt_book_macroman/stylesheet.css'
-// import './fonts/Webfonts/futurapt_bookitalic_macroman/stylesheet.css'
-// import './fonts/Webfonts/futurapt_demi_macroman/stylesheet.css'
-// import './fonts/Webfonts/futurapt_demiitalic_macroman/stylesheet.css'
-// import 'typeface-space-mono'
-// import 'typeface-spectral'
 
 export { default as theme } from 'gatsby-mdx-theme'
 export default [
   ...intro,
   ...gatsbyOverview,
-  ...newFeatures,
+  ...features,
   ...demo,
   ...qAndA,
   ...conclusion
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -8,6 +8,19 @@ module.exports = {
       {
         test: /\.(woff(2)?|ttf|eot|svg)/,
         use: ['file-loader']
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true // webpack@2.x and newer
+            }
+          }
+        ]
       }
     ]
   }
